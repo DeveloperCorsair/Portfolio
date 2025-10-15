7c7176001e6149f68d2e21de5af356ad330d5ec9
henrique <henriquemartins7t@gmail.com>
2025-10-14 23:44:02 -0300

    Implementação do FlowBite! E inicio do Dark Mode.

---
+++ src/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./pages/**/*.{html,js}",
+    "./js/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
