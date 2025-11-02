81ca0f8d268fc8e7caaa86c8ed9e9c4706f1e2b6
henrique <henriquemartins7t@gmail.com>
2025-11-02 00:34:12 -0300

    Implementado dark mode na home.

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import flowbite from "flowbite/plugin";
 export default {
   darkMode: "class",
   content: [
     "./index.html",
-    "./pages/**/*.{html,js}",
-    "./js/**/*.{html,js}",
+    "./src/pages/**/*.{html,js}",
+    "./src/js/**/*.{html,js}",
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbite],
 };
